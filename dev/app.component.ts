8dc2ba5405a55fcc3380c0220a9a533fdbc35065
Techiebud (Mac) <techiebud@gmail.com>
2016-02-19 09:49:05 -0500

    Lesson Finished.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,9 +1,31 @@
 import {Component} from 'angular2/core';
+import {InputComponent} from "./bindings/input.component";
+import {ConfirmComponent} from "./bindings/confirm.component";
 
 @Component({
     selector: 'app',
-    template: 'Hello World!',
+    template: `
+
+         <div class="container"><my-input (submitted)="onSubmit($event)" [myself]="confirmMyself"></my-input></div>
+         <div class="container"><my-confirm (confirmed)="onConfirm($event)" [myself]="myself"></my-confirm></div>
+    `,
+    directives: [InputComponent, ConfirmComponent]
 })
 export class AppComponent {
 
+    myself = {name: '', age: ''};
+    confirmMyself  = {name: '', age: ''}
+
+    onSubmit(myself:{name: string, age: string}) {
+
+        this.myself = myself;
+
+
+    }
+
+    onConfirm(myself: {name: string, age: string}) {
+
+        this.confirmMyself = myself;
+    }
 }
+
